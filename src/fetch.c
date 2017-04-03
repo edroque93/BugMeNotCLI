32a1b4c47c13f8a23c4f6fd7b75991cdd86376e1
edroque93 <quique.lpgc@gmail.com>
2017-04-03 16:10:15 +0200

    Fetch rework

--- src/fetch.c
+++ src/fetch.c
@@ -5,25 +5,23 @@ size_t write_callback(void *ptr, size_t size, size_t nmeb, void *userdata) {
   size_t rsize = size * nmeb;
 
   if (rsize) {
-    stream->contents =
-        (char *)realloc(stream->contents, stream->length + rsize + 1);
-    memcpy(stream->contents + stream->length, ptr, rsize);
+    stream->raw = (char *)realloc(stream->raw, stream->length + rsize + 1);
+    memcpy(stream->raw + stream->length, ptr, rsize);
     stream->length += rsize;
-    stream->contents[stream->length] = 0;
+    stream->raw[stream->length] = 0;
   }
 
   return rsize;
 }
 
-char *fetch_webpage(char *url) {
-  webpage web = {.contents = NULL, .length = 0};
+void fetch_webpage(webpage *web) {
+  web->raw = NULL;
+  web->length = 0;
 
   CURL *handle = curl_easy_init();
-  curl_easy_setopt(handle, CURLOPT_URL, url);
+  curl_easy_setopt(handle, CURLOPT_URL, web->URL);
   curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_callback);
-  curl_easy_setopt(handle, CURLOPT_WRITEDATA, &web);
+  curl_easy_setopt(handle, CURLOPT_WRITEDATA, web);
   curl_easy_perform(handle);
   curl_easy_cleanup(handle);
-
-  return web.contents;
 }
--- src/fetch.h
+++ src/fetch.h
@@ -1,16 +1,11 @@
 #ifndef FETCH_H
 #define FETCH_H
 
+#include "common.h"
 #include <curl/curl.h>
-#include <stdlib.h>
 #include <string.h>
 
-typedef struct {
-  char *contents;
-  size_t length;
-} webpage;
-
 size_t write_callback(void *ptr, size_t size, size_t nmeb, void *userdata);
-char *fetch_webpage(char *url);
+void fetch_webpage(webpage *web);
 
 #endif
