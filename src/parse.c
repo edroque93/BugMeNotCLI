1d363c613924279be6eb843f18859991de4487fe
edroque93 <quique.lpgc@gmail.com>
2017-04-03 16:10:22 +0200

    Parse rework

--- src/parse.c
+++ src/parse.c
@@ -1,1 +1,28 @@
+#include "parse.h"
 
+void print_element_names(xmlNode *node) {
+  xmlNode *cur_node = NULL;
+
+  for (cur_node = node; cur_node; cur_node = cur_node->next) {
+    if (cur_node->type == XML_ELEMENT_NODE) {
+      printf("%s\n", cur_node->name);
+    }
+
+    print_element_names(cur_node->children);
+  }
+}
+
+void parse_webpage(webpage *web) {
+  htmlDocPtr doc =
+      htmlReadMemory(web->raw, web->length, web->URL, NULL, HTML_FLAGS);
+  if (doc == NULL) {
+    print_error("parse_webpage::htmlReadMemory returned NULL.");
+  } else {
+    xmlNode *root_element = NULL;
+    root_element = xmlDocGetRootElement(doc);
+    printf("%s\n", root_element->name);
+    print_element_names(root_element);
+    xmlFreeDoc(doc);
+    xmlCleanupParser();
+  }
+}
--- src/parse.h
+++ src/parse.h
@@ -1,6 +1,14 @@
 #ifndef PARSE_H
 #define PARSE_H
 
-#include <libxml/tree.h>
+#include "common.h"
+#include <libxml/HTMLparser.h>
+
+#define HTML_FLAGS                                                             \
+  (HTML_PARSE_NOBLANKS | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING |           \
+   HTML_PARSE_NONET)
+
+void print_element_names(xmlNode *node);
+void parse_webpage(webpage *web);
 
 #endif
