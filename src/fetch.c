c0a691711e10d11e166c5bb02200fb0f21dae4fa
edroque93 <quique.lpgc@gmail.com>
2017-04-05 11:44:24 +0200

    Stupid curl does not free everything it uses. Reeeeeee

--- src/fetch.c
+++ src/fetch.c
@@ -18,10 +18,15 @@ void fetch_webpage(webpage *web) {
   web->raw = NULL;
   web->length = 0;
 
+  CURLcode error = curl_global_init(CURL_GLOBAL_DEFAULT);
+  if (error) {
+    print_error("fetch_webpage::curl_global_init returned error.");
+  }
   CURL *handle = curl_easy_init();
   curl_easy_setopt(handle, CURLOPT_URL, web->URL);
   curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_callback);
   curl_easy_setopt(handle, CURLOPT_WRITEDATA, web);
   curl_easy_perform(handle);
   curl_easy_cleanup(handle);
+  curl_global_cleanup();
 }
